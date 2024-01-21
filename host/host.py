ee298d99a1a3adff24d00ed89e42c5f14c4c4b90
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2024-01-20 22:04:59 -0500

    create server file. TCP server listener. Fixed. fixed invalid port.

--- host/host.py
+++ host/host.py
@@ -2,10 +2,10 @@ import socket
 
 if __name__ == "__main__":
     ip = "127.0.0.1"
-    port = "5000"
+    port = 8080
 
     client = socket.socket()
-    client.connect(ip, port)
+    client.connect((ip, port))
     message = "Hello.".encode("utf-8")
     client.send(message)
     response = client.recv(1024)
--- server/server.py
+++ server/server.py
@@ -1,2 +1,35 @@
+import socket
+
+#inilizes socket
+sock = socket.socket()
+
+PORT = 8080
+
+def startup():
+    #starts server on 127.0.0.1:5000
+    sock.bind(('127.0.0.1',PORT))
+
+    #listens for 5 connections
+    sock.listen(5)
+
+    run()
+
+def run():
+    while 1:
+        connection, address = sock.accept() #accepts incoming connection
+
+        print("\n[Recieved Connection] ",address)
+
+        #recieving request
+        request = connection.recv(1024) #receives 1024 bytes
+        msg = request.decode('utf-8')
+        print(msg)
+
+        response = "You are connected!\n".encode('utf-8')
+        connection.send(response)
+        connection.close()
+
 if __name__ == "__main__":
-    print("Hello Server!")
+    print("Hello From Server")
+
+    startup()
