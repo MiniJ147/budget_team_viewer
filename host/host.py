4f5e8eb91e8f2ca6a6006617ae529754a7da7db9
John Lewis <johnlewis@ufl.edu>
2024-01-20 22:22:22 -0500

    fixed syntax

--- host/host.py
+++ host/host.py
@@ -1,14 +1,26 @@
 import socket
 
-if __name__ == "__main__":
-    ip = "127.0.0.1"
-    port = 8080
+ip = "127.0.0.1"
+port = 8080
 
+def startup():
+    # creates a new socket
     client = socket.socket()
+    
+    # connects to the server
     client.connect((ip, port))
+    
+    # sends a messgae to the server
     message = "Hello.".encode("utf-8")
     client.send(message)
+    
+    # recivees and prints a message from the server
     response = client.recv(1024)
     print(response.decode("utf-8"))
+    
+    #closes the connection
     client.close()
+
+if __name__ == "__main__":
+    startup()
 
