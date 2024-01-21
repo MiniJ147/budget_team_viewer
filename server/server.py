058827748834f896b4b3dbfbc4bc850f614b114c
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2024-01-20 22:34:44 -0500

    basic demo of how sending clients ips to thehost will work

--- server/server.py
+++ server/server.py
@@ -5,6 +5,8 @@ sock = socket.socket()
 
 PORT = 8080
 
+EXAMPLE_DATA = ['68.129.180.01','28.400.180.01','70.129.120.01']
+
 def startup():
     #starts server on 127.0.0.1:5000
     sock.bind(('127.0.0.1',PORT))
@@ -20,13 +22,14 @@ def run():
 
         print("\n[Recieved Connection] ",address)
 
-        #recieving request
-        request = connection.recv(1024) #receives 1024 bytes
-        msg = request.decode('utf-8')
-        print(msg)
+        #send the size of current connections
+        number_of_connections = len(EXAMPLE_DATA)
+        connection.send(number_of_connections.to_bytes(2,"big")) #sending int as bytes
 
-        response = "You are connected!\n".encode('utf-8')
-        connection.send(response)
+        #loops through every ip in connected and sends to the host
+        for ip in EXAMPLE_DATA:
+            connection.send(ip.encode('utf-8'))
+
         connection.close()
 
 if __name__ == "__main__":
---
+++ windows/run.bat
@@ -0,0 +1,1 @@
+:: will have code to run startup
