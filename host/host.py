88d730650b3d5fe380708c1584257094b22bb152
John Lewis <johnlewis@ufl.edu>
2024-01-20 21:56:30 -0500

    created basic host file and socket connection

--- host/host.py
+++ host/host.py
@@ -1,2 +1,14 @@
+import socket
+
 if __name__ == "__main__":
-    print("Hello host!")
+    ip = "127.0.0.1"
+    port = "5000"
+
+    client = socket.socket()
+    client.connect(ip, port)
+    message = "Hello.".encode("utf-8")
+    client.send(message)
+    response = client.recv(1024)
+    print(response.decode("utf-8"))
+    client.close()
+
